5e776be3f5005f04b004e19efde571a4e4cfa849
ahmedqadry7 <ahmedqadry72@gmail.com>
2024-04-08 14:51:41 +0200

    updates

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,7 +8,13 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Movies App"),
+        backgroundColor: Colors.black,
+        title: Container(
+            height: 50,
+            width: 200,
+            child: Lottie.network(
+                "https://lottie.host/19e350a3-71fb-41e6-af52-a69cac311867/rvj70SfJEh.json")),
+        centerTitle: true,
       ),
     );
   }
